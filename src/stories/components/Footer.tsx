b1caea3873270114defa2e478be4e80632b52790
Junior Kissi <juniorkissi@MacBookPro.attlocal.net>
2024-11-04 16:40:06 -0600

    fix

---
+++ src/stories/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+const Footer = () => {
+ 
+
+  return (
+    <div>
+      <h1>Copyright info@2024</h1>
+      
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/stories/components/layout/Footer.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Footer from './Footer';
+
+const meta: Meta<typeof Footer> = {
+  title: 'Components/layout/Footer',
+  component: Footer,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+  args: {
+    email: 'info@xyzbank.com',
+    year: 2024,
+    backgroundColor: '#3b82f6', // Default background color
+    textColor: 'white', // Default text color
+  },
+};
+
+// Example of a custom variant
+export const CustomFooter: Story = {
+  args: {
+    email: 'support@xyzbank.com',
+    year: 2024,
+    backgroundColor: '#1d4ed8', // Custom background color
+    textColor: 'yellow', // Custom text color
+  },
+};
